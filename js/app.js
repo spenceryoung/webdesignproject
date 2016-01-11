872aa37df82057a23e151f5199cf628174527ca8
spenceryoung <syoung42788@gmail.com>
2016-01-11 05:13:48 -0600

    add files

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('myApp', [
+    'ngRoute',
+    'MyApp.controllers'
+]);
+
+app.config([
+    '$routeProvider',
+    function($routeProvider) {
+
+        $routeProvider
+        .when('/one', {
+            templateUrl: 'partials/one.html',
+            controller: 'OneCtrl'
+        })
+        .when('/two', {
+            templateUrl: 'partials/two.html',
+            controller: 'TwoCtrl'
+        })
+        .when('/three', {
+            templateUrl: 'partials/three.html',
+            controller: 'ThreeCtrl'
+        })
+        .when('/four', {
+            templateUrl: 'partials/four.html',
+            controller: 'FourCtrl'
+        })
+        .when('/five', {
+            templateUrl: 'partials/five.html',
+            controller: 'FiveCtrl'
+        })
+        .otherwise({
+            redirectTo: '/one'
+        });
+    }
+]);
---
+++ js/controller.js
@@ -0,0 +1,25 @@
+angular.module("MyApp.controllers", [])
+    .controller("MainController", ['$scope',
+        function($scope) {
+
+        }])
+    .controller('OneCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+
+        }])
+    .controller('TwoCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+
+        }])
+    .controller('ThreeCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+
+        }])
+    .controller('FourCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+
+        }])
+    .controller('FiveCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+
+        }])
