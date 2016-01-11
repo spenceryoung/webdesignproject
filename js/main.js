3ddd9b39b322211845100ddd0dadfccf436d6229
spenceryoung <syoung42788@gmail.com>
2016-01-11 05:16:25 -0600

    adding pages

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// ==========  Mobile Navigation  ==========
+
+$(document).ready(function() {
+
+    $( '#side-menu-btn' ).click(function() {
+        $('#side-menu').slideToggle();
+    });
+
+});
+
