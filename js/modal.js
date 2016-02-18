b582721b58eb767c83ecf5a1da01174ad20f3f28
spenceryoung <syoung42788@gmail.com>
2016-02-18 07:22:30 -0600

    push

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".fancybox").fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+        helpers : {
+            overlay : {
+                locked : false,
+                css : {
+                    'background' : 'rgba(0, 0, 0, 0.5)'
+                }
+        }
+    }
+    });
+});
